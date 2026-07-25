5658908afb20def1def495b37759b58a1d8077a4
Peyton Rivers <peytonrivers716@gmail.com>
2026-07-25 00:35:37 -0400

    everything in regards to the omniparser and applyr process being coherint, next fix is the ram issue??

--- util/utils.py
+++ util/utils.py
@@ -22,7 +22,7 @@ from paddleocr import PaddleOCR
 # reader = easyocr.Reader(['en'])
 paddle_ocr = PaddleOCR(
     lang='en',  # other lang also available
-    use_angle_cls=False,
+    use_angle_cls=True,
     use_gpu=False,  # using cuda will conflict with pytorch in the same process
     show_log=False,
     max_batch_size=1024,
